0f42c74a2a0add47db367d72d92d0d1d3b6b9660
N-Santiago <norbertosantiago82@gmail.com>
2021-05-11 19:49:03 -0500

    models and db migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,24 +15,6 @@ ActiveRecord::Schema.define(version: 2021_05_12_001915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "albums", force: :cascade do |t|
-    t.string "name"
-    t.string "artist"
-    t.string "genre"
-    t.string "condition"
-    t.string "description"
-    t.integer "price"
-    t.integer "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "episodes", force: :cascade do |t|
     t.string "audio"
     t.string "image"
