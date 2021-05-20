fd514d2d4c38fedbd761ddfab7661d72b7c63e0a
N-Santiago <norbertosantiago82@gmail.com>
2021-05-19 19:13:13 -0500

    episodes controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :podcasts, only: [:index, :create]
+  resources :episodes, only: [:index, :create]
 end
