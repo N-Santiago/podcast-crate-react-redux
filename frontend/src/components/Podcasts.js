22b6e88f76bfb551c56a5b638a2830d86dbb199f
N-Santiago <norbertosantiago82@gmail.com>
2021-05-30 16:58:00 -0500

    add image to podcast component

--- frontend/src/components/Podcasts.js
+++ frontend/src/components/Podcasts.js
@@ -4,6 +4,7 @@ const Podcasts = (props) => {
     return (
         <div>
             <h2>{props.podcast.title}</h2>
+            <p><img src={props.podcast.image}></img></p>
             <p>{props.podcast.website}</p>
         </div>
     );
