b8dec97bc9f43d5eef164b215290ab2f28080efb
N-Santiago <norbertosantiago82@gmail.com>
2021-08-16 18:05:53 -0500

    Generated devise user model, controller and migrated db

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -37,6 +37,17 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
+end
+
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
@@ -263,7 +274,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = []
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-podcasts = Podcast.create([{title: 'WTF With Marc Maron', website: 'wtfpod.com'}])
+# podcasts = Podcast.create([{title: 'WTF With Marc Maron', website: 'wtfpod.com'}])
