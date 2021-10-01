c807d6a47c76a9d1b47f763085f53ff36b6e3664
N-Santiago <norbertosantiago82@gmail.com>
2021-09-30 21:12:28 -0500

    changes to model validations

--- backend/app/models/podcast.rb
+++ backend/app/models/podcast.rb
@@ -3,7 +3,7 @@ class Podcast < ApplicationRecord
     belongs_to :user
     has_many :episodes
     has_one_attached :image
-    validates :title, :website, presence: true
+    validates :title, :website, presence: true, length: { in: 3..35, too_long: "%{count} characters is the minimum and maximum allowed" }
 
     def image_url
         url_for(self.image)
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :podcasts, dependent: :destroy
-        #  validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}             
+         validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}             
 end
