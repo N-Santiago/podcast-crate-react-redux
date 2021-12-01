0aa9e0d8bb03b5290561b25f90b293dc3c13b8e5
N-Santiago <norbertosantiago82@gmail.com>
2021-11-30 22:02:13 -0600

    Updated New Episode Form

--- frontend/src/components/EpisodeInput.js
+++ frontend/src/components/EpisodeInput.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addEpisode } from '../actions/addEpisode';
-import Button from '@material-ui/core/Button';
+import { Box, Button } from '@material-ui/core/';
 
 class EpisodeInput extends React.Component {
 
@@ -31,19 +31,27 @@ class EpisodeInput extends React.Component {
                 <div>
                     <Button color="inherit" href={`/podcasts/${this.props.podcast.id}/edit`}><h2>Edit Podcast</h2></Button>
                 </div>
-                <h2>Enter an Episode</h2>
+                <Box display="flex" justifyContent="center">
                 <form onSubmit={this.handleSubmit}>
+                <h2>Enter an Episode</h2>
+                <fieldset>
                     <label>Episode Name:</label>
                     <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange} /><br /><br />
+                </fieldset>
+                <fieldset>
                     <label>Date:</label>
                     <p><i><small>In order to keep stuff as accurate as possible, please enter date episode was published on Spotify.</small></i></p>
                     <input type='date' placeholder='Date' value={this.state.date} name='date' onChange={this.handleChange} /><br /><br />
+                </fieldset> 
+                <fieldset>   
                     <label>Spotify Embedded URL:</label>
                     <p><i><small>Spotify's iframe code is hardcoded. Go to copy embedded link on the Spotify's podcast and only copy the url on that code.</small></i></p> 
                     <p><i><small>Hope by complicating things, this turn out to be fun.</small></i></p>
                     <input type='text' placeholder='Spotify Embedded URL' value={this.state.spotify_link} name='spotify_link' onChange={this.handleChange} /><br /><br />
-                    <input type="submit" value="Submit" /> 
-                </form><br/>
+                </fieldset><br/>    
+                    <Button variant="contained" size="small" color="inherit" type='submit'>Submit</Button>
+                </form>
+                </Box><br/>
             </div>
         )
     }
